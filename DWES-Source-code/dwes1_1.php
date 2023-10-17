c377286919da1e4ed5eacac5466fcd802e44041a
Torremolinos <adrian_2054@hotmail.com>
2023-10-17 12:13:47 +0200

    primer php

---
+++ DWES-Source-code/dwes1_1.php
@@ -0,0 +1,20 @@
+<!--Nuestra primera instruccion en PHP!!!-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo "Hello World";
+            ?>
+        </h1>
+        <p>
+            <?php
+            print "Pinta Bien";
+            ?>
+        </p>
+    </body>
+</html>
