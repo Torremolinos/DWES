098e27a4ac1e8266e744fb28a79ef2a8bf86957f
Torremolinos <adrian_2054@hotmail.com>
2023-10-18 12:03:12 +0200

    Cambios al hello world

---
+++ DWES-Source-code/dwes1_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+</head>
+<body>
+    <h1>
+        <?php
+        echo "Hello world!";
+        ?>
+    </h1>
+    <p>
+        <?php
+        $saludo = "Bienvenido";
+        $nombre = "Pepe";
+        echo $saludo . ", " . $nombre;
+       ?>
+    </p>
+</body>
+</html>
